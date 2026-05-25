b8100bda8ba69c182116563449efa65dd640d2aa
aakashjois110 <aakashjois110@gmail.com>
2026-05-25 22:59:04 +0800

    Create checkout.js

---
+++ api/checkout.js
@@ -0,0 +1,21 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+module.exports = async (req, res) => {
+  const session = await stripe.checkout.sessions.create({
+    line_items: [
+      {
+        price_data: {
+          currency: 'aud',
+          product_data: { name: 'Your Product Name' },
+          unit_amount: 2000, // $20.00 in cents
+        },
+        quantity: 1,
+      },
+    ],
+    mode: 'payment',
+    success_url: 'https://yoursite.com/success',
+    cancel_url: 'https://yoursite.com',
+  });
+
+  res.redirect(303, session.url);
+};
